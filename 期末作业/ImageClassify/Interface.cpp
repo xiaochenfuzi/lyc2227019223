fd1f6d783330fded52948a9536f2311939d832f9
liuyuchen <1163865418@qq.com>
2023-06-25 20:52:35 +0800

    期末作业

---
+++ 期末作业/ImageClassify/Interface.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "Interface.h"
+
+CInterface::CInterface()
+{
+
+}
+
+CInterface::~CInterface()
+{
+
+}
+
+void CInterface::ImageClassify(string strPath)
+{
+
+}
+
---
+++ 期末作业/ImageClassify/LoginDlg.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+
+// LoginDlg 对话框
+
+class LoginDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(LoginDlg)
+
+public:
+	LoginDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~LoginDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LOGIN };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton1();
+	CString m_username;
+	CString m_password;
+};
