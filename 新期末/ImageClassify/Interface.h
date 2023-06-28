b5505267141ae589ecd03bbfc5cf4ea8d65953f6
liuyuchen <1163865418@qq.com>
2023-06-28 20:11:55 +0800

    新期末

---
+++ 新期末/ImageClassify/Interface.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+using namespace std;
+class CInterface
+{
+	CInterface();
+	~CInterface();
+	void ImageClassify(string strPath);
+};
+
---
+++ 新期末/ImageClassify/LoginDlg.cpp
@@ -0,0 +1,59 @@
+﻿// LoginDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "ImageClassify.h"
+#include "LoginDlg.h"
+#include "afxdialogex.h"
+
+
+// LoginDlg 对话框
+
+IMPLEMENT_DYNAMIC(LoginDlg, CDialogEx)
+
+LoginDlg::LoginDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_LOGIN, pParent)
+	, m_username(_T(""))
+	, m_password(_T(""))
+{
+
+}
+
+LoginDlg::~LoginDlg()
+{
+}
+
+void LoginDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_username);
+	DDX_Text(pDX, IDC_EDIT2, m_password);
+}
+
+
+BEGIN_MESSAGE_MAP(LoginDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON2, &LoginDlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON1, &LoginDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// LoginDlg 消息处理程序
+
+
+void LoginDlg::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	exit(0);
+}
+
+
+void LoginDlg::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	UpdateData();
+	if (m_username != _T("3057398047") || m_password !=_T( "jie")) {
+		AfxMessageBox(_T("账号或者密码不正确!"));
+		return;
+	}
+	EndDialog(1);
+}
